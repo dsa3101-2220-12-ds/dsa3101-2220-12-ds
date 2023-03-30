bb94d2173e1e9216e9e640e283d002bbc1e251e9
yeoyingxin <yeoyingxin01@gmail.com>
2023-03-30 12:12:17 +0800

    Updated deprecated packages

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,6 +1,7 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
+from dash import Dash
 from dash.dependencies import Input, Output
 import plotly.express as px
 import dash_bootstrap_components as dbc
