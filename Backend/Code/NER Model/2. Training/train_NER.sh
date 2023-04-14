865478c15229e10e56362a337b5d144f8c2deeb7
Ernest Liu <ernest.liu@sembcorp.com>
2023-04-14 13:39:50 +0800

    Edited code for training the NER model and Word2Vec model to reflect changes in the directory structure

--- Backend/Code/NER Model/2. Training/train_NER.sh
+++ Backend/Code/NER Model/2. Training/train_NER.sh
@@ -1,3 +1,2 @@
 # RUN THIS IN COMMAND LINE: bash train_NER.sh
-python -m spacy train config.cfg --output ./ --paths.train ./../../Data/NER_annotated_data/StackOverflow/train.spacy --paths.dev ./../../Data/NER_annotated_data/StackOverflow/dev.spacy
-
+python -m spacy train "./../Training Pipeline/config.cfg" --output ./../ --paths.train ../../../Data/NER_annotated_data/Job_Mod_Descriptions/mod_annotations.spacy --paths.dev ../../../Data/NER_annotated_data/ChatGPT/annotations.spacy --paths.vectors "./../Training Pipeline/"
